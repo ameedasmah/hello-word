d50a1696130d9f0fde1f1b7a62f7c5608d0fdac7
ameedasmah <ameed.asmah@hotmail.com>
2020-08-20 12:56:56 +0300

    adding a subtract function

--- hello.js
+++ hello.js
@@ -2,3 +2,9 @@ console.log("Hello, World");
  function sum (num1, num2) {
   return num1 + num2;
 }
+  function sum (num1, num2) {
+  return num1 + num2;
+  }
+  function subtract (num1, num2) {
+  return num1 - num2;
+   }
