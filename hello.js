708beb02ccb39da36793f05b99852a22151ba1b7
ameedasmah <ameed.asmah@hotmail.com>
2020-08-20 12:57:45 +0300

    Added a comment

--- hello.js
+++ hello.js
@@ -8,3 +8,11 @@ console.log("Hello, World");
   function subtract (num1, num2) {
   return num1 - num2;
    }
+      // this function add two numbers
+  function sum (num1, num2) {
+  return num1 + num2;
+  }
+    // this function subtract two numbers
+  function subtract (num1, num2) {
+  return num1 - num2;
+   }
